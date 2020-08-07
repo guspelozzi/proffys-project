956e92b38f32afcdd64dd8c695b41022d5b2d3de
Gustavo Cesar <gustavopelozzi13@gmail.com>
2020-08-07 01:28:22 -0300

    Quase finalizando o frontend web

--- web/src/pages/TeachesList/index.tsx
+++ web/src/pages/TeachesList/index.tsx
@@ -2,26 +2,42 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import './styles.css';
 import TeacherItem from '../../components/TeacherItem';
+import Select from '../../components/Select';
 
 export default function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
 
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                        ]}
+                    />
+                    <Select 
+                        name="week_day" 
+                        label="Dia da semana"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                        ]}
+                    />
+                    <Select 
+                        name="time" 
+                        label="Hora"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                        ]}
+                    />
 
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
                 </form>
             </PageHeader>
 
