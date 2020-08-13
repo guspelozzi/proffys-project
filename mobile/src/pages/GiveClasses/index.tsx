7e72eecc704872f081b2383714bf954fde83b306
Gustavo Pelozzi <53912182+guspelozzi@users.noreply.github.com>
2020-08-12 23:43:26 -0300

    Criação da parte mobile do projeto

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import styles from './styles';
+
+function GiveClasses() {
+    return (
+        <View />
+    );
+}
+
+export default GiveClasses;
---
+++ mobile/src/routes/AppStack.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+import LandingPage from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
+const { Navigator, Screen } = createStackNavigator();
+
+function AppStack() {
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen name="LandingPage" component={LandingPage}/>
+                <Screen name="GiveClasses" component={GiveClasses}/>
+            </Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default AppStack;
